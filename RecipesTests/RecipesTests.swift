80bdd0c138ef9883d6145855d7364c5329bbc325
Spencer Curtis <spencer.curtis@luma-touch.com>
2025-02-05 20:46:15 -0700

    Implement initial fetching process and write tests for known scenarios thanks to the test urls

--- RecipesTests/RecipesTests.swift
+++ RecipesTests/RecipesTests.swift
@@ -7,11 +7,32 @@
 
 import Testing
 @testable import Recipes
+import Foundation
 
 struct RecipesTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    
+    let malformedJSONURL = URL(string: "https://d3jbb8n5wk0qxi.cloudfront.net/recipes-malformed.json")!
+    let emptyRecipesJSONURL = URL(string: "https://d3jbb8n5wk0qxi.cloudfront.net/recipes-empty.json")!
+    
+    @Test func testFetchAllRecipes() async throws {
+        let recipeService = RecipesService()
+        let recipes = try await recipeService.fetchAllRecipes(with: recipeService.recipeURL)
+        #expect(recipes.count > 0)
+    }
+    
+    @Test func testMalformedJSON() async throws {
+        let recipeService = RecipesService()
+        
+        // Test that a decoding error is thrown when fetching malformed JSON
+        await #expect(throws: DecodingError.self) {
+            _ = try await recipeService.fetchAllRecipes(with: malformedJSONURL)
+        }
+    }
+    
+    @Test func testEmptyJSON() async throws {
+        let recipeService = RecipesService()
+        let recipes = try await recipeService.fetchAllRecipes(with: emptyRecipesJSONURL)
+        #expect(recipes.count == 0)
     }
 
 }
